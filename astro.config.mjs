0eb3f1b0d0744761959baa53d97e18f56170c2ec
tgimgcdn <imgcdn.rr.nu@gassky.eu.org>
2025-05-04 10:20:37 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,10 +25,6 @@ export default defineConfig({
                 },
               })
             ],
-            components: {
-              // 添加自定义Head组件
-              Head: './src/components/Head.astro',
-            },
 			title: 'My Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
