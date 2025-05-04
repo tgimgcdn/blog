4ab40e985e429580f08c23954b2810e848acd7a1
tgimgcdn <imgcdn.rr.nu@gassky.eu.org>
2025-05-04 11:20:24 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,46 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import starlightThemeFlexoki from 'starlight-theme-flexoki';
-import starlightImageZoom from 'starlight-image-zoom';
-import starlightBlog from 'starlight-blog';
+import starlightThemeFlexoki from 'starlight-theme-flexoki'
+import starlightImageZoom from 'starlight-image-zoom'
+import starlightBlog from 'starlight-blog'
 
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://canjie.ggff.net',
-  integrations: [
-    starlight({
-      plugins: [
-        starlightImageZoom(),
-        starlightThemeFlexoki(),
-        starlightBlog({
-          authors: {
-            cmssky: {
-              name: 'cmssky',
-              title: '网站管理员',
-              picture: 'https://cdn.canjie.org/avatar2.jpg',
-              url: 'https://canjie.org',
-            },
-          },
-          prevNextLinksOrder: 'reverse-chronological',
-        }),
-      ],
-      title: 'My Docs',
-      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-      sidebar: [
-        {
-          label: 'Guides',
-          items: [{ label: 'Example Guide', slug: 'guides/example' }],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
-        },
-      ],
-    }),
-  ],
+	site: 'https://canjie.ggff.net',
+	integrations: [
+	      starlight({
+	        plugins: [
+		starlightImageZoom(),
+		starlightThemeFlexoki(),
+                starlightBlog({
+                authors: {
+                  cmssky: {
+                    name: 'cmssky',
+                    title: '网站管理员',
+                    picture: 'https://cdn.canjie.org/avatar2.jpg',
+                    url: 'https://canjie.org',
+                  },
+                },
+				prevNextLinksOrder: 'reverse-chronological',
+              })
+            ],
+			title: 'My Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
 });
