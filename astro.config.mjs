c91d74f39eedca37835f4d2a7604d4c6a71daa03
tgimgcdn <imgcdn.rr.nu@gassky.eu.org>
2025-05-09 10:22:48 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -68,7 +68,7 @@ export default defineConfig({
 					reactionsEnabled: '1',
 					emitMetadata: '0',
 					inputPosition: 'bottom',
-					theme: 'dark',
+					theme: 'dark_dimmed',
 					lang: 'zh-CN',
 					loading: 'lazy',
 					crossorigin: 'anonymous'
