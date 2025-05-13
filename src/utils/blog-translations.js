89c4b4870fb380b27d866e34b2821b9d081aaf2c
tgimgcdn <imgcdn.rr.nu@gassky.eu.org>
2025-05-13 12:49:11 +0800

    Create blog-translations.js

---
+++ src/utils/blog-translations.js
@@ -0,0 +1,25 @@
+/**
+ * starlight-blog 的中文翻译
+ */
+export const StarlightBlogTranslations = {
+  zh: {
+    'starlightBlog.authors.count_one': '{{author}}的{{count}}篇文章',
+    'starlightBlog.authors.count_other': '{{author}}的{{count}}篇文章',
+    'starlightBlog.pagination.prev': '较新文章',
+    'starlightBlog.pagination.next': '较早文章',
+    'starlightBlog.post.date': '{{date, datetime(dateStyle: medium)}}',
+    'starlightBlog.post.lastUpdate':
+      ' - 最后更新: <time datetime="{{isoDate}}">{{date, datetime(dateStyle: medium)}}</time>',
+    'starlightBlog.post.draft': '草稿',
+    'starlightBlog.post.featured': '精选',
+    'starlightBlog.post.tags': '标签:',
+    'starlightBlog.sidebar.all': '所有文章',
+    'starlightBlog.sidebar.featured': '精选文章',
+    'starlightBlog.sidebar.recent': '最近文章',
+    'starlightBlog.sidebar.tags': '标签',
+    'starlightBlog.sidebar.authors': '作者',
+    'starlightBlog.sidebar.rss': 'RSS订阅',
+    'starlightBlog.tags.count_one': '带有标签"{{tag}}"的{{count}}篇文章',
+    'starlightBlog.tags.count_other': '带有标签"{{tag}}"的{{count}}篇文章',
+  }
+}; 
